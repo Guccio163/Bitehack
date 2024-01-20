f6abb3474afedfa4f8c6c7f1e43d909bd8b73ef6
marek <marekzuwala@gmail.com>
2024-01-20 14:31:14 +0100

    registration and login

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,79 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
-# Create your models here.
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, first_name, last_name, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address")
+        if not username:
+            raise ValueError("Users must have username")
+        if not first_name:
+            raise ValueError("Users must have username")
+        if not last_name:
+            raise ValueError("Users must have username")
+        user = self.model(
+            email=email,
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, first_name, last_name, password=None,  **extra_fields):
+        user = self.create_user(
+            email=email,
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            password=password)
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(db_index=True, unique=True, max_length=255)
+    username = models.CharField(max_length=255, db_index=True, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    date_joined = models.DateTimeField(
+        verbose_name='date joined', auto_now_add=True)
+
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return True
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,38 @@
-from django.http import HttpResponse
-from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+from .models import User
+from .serializers import RegisterSerializer, AuthTokenSerializer
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
 from rest_framework.decorators import api_view
 
 # Create your views here.
 @api_view()
 def test(request):
     return Response("No siemka")
+
+class CustomAuthToken(ObtainAuthToken, generics.CreateAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = AuthTokenSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                       context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+
+        return Response({
+            'token': token.key,
+            'username': user.username,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+        })
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer

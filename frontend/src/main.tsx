a4505df4972642a73b5352b44cd1f3dcb46acc78
Wiktor Gut <411761@student.agh.edu.pl>
2024-01-20 18:12:30 +0100

    router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import MainPage from './pages/MainPage.tsx';
+import SettingsPage from './pages/SettingsPage.tsx';
+import ProfilePage from './pages/ProfilePage.tsx';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/settings",
+    element: <SettingsPage />,
+  },
+  {
+    path: "/profile",
+    element: <ProfilePage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
+
+
